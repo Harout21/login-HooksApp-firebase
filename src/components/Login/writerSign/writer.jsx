6742b771a11b1a1f0c4aeeb64a260b5e71c2efd1
Harout <harut2121@gmail.com>
2020-08-27 14:43:55 +0400

    history remake

--- src/components/Login/writerSign/writer.jsx
+++ src/components/Login/writerSign/writer.jsx
@@ -60,7 +60,7 @@ class Typewriter extends React.Component {
         return (
             <div className="typewriter">
                 <div className="typewriter__type">
-                    {!text ? "":"Hello"}{text}
+                    {!text ? "":"Hello"+" "}{text}
                 </div>
                 <div className="typewriter__cursor"/>
             </div>
